6f72af215849b069758d7abf62093fc92c3fd46d
Georg Brandl <georg.brandl@frm2.tum.de>
2012-06-07 21:17:51 +0200

    Add error display for "new context" form.

--- yamoda/server/views/__init__.py
+++ yamoda/server/views/__init__.py
@@ -23,6 +23,7 @@ from yamoda.server.database import Context, User, Set
 @login_required
 def contexttable():
     """shows every context in the database"""
+    error = None
     if request.method == 'POST':
         name = request.form['ctx_name']
         brief = request.form['ctx_brief']
@@ -36,12 +37,13 @@ def contexttable():
             db.session.commit()
         except (ValueError, IntegrityError):
             db.session.rollback()
-            error = 'Invalid Input.'
+            error = 'Invalid input.'
         else:
             flash('Created new context: {0}'.format(name))
 
     contextlist = Context.query.all()
-    return render_template('contexttable.html', contextlist=contextlist)
+    return render_template('contexttable.html', contextlist=contextlist,
+                           error=error)
 
 
 @app.route('/context/<ctx_name>')
