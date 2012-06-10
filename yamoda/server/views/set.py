ecce3712affcbe2c9ed4c04cde6c91900eccc11b
Georg Brandl <georg.brandl@frm2.tum.de>
2012-06-10 23:36:23 +0200

    Fix the data_view view.

--- yamoda/server/views/set.py
+++ yamoda/server/views/set.py
@@ -6,17 +6,20 @@
 Data set related views.
 """
 
-from flask import render_template
-from flask.ext.login import login_required
+from flask import render_template, request
+from flask.ext.login import login_required, current_user
 
 from yamoda.server import app
 from yamoda.server.database import Set
 
 
-@app.route('/data')
+@app.route('/data/<which>')
 @login_required
-def data_view(all=False):
-    setlist = Set.query.all()
+def data_view(which='mine'):
+    if which == 'all':
+        setlist = Set.query.all()
+    else:
+        setlist = Set.query.filter_by(user=current_user)
     for s in setlist:
         print s.readable()
     return render_template('setlist.html', sets=setlist)
