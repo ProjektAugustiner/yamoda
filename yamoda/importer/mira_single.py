6c6823fb243e1dd4b15e96f6121a116aadc71a26
Georg Brandl <georg@python.org>
2012-06-26 18:56:00 +0200

    Add some standard MIRA devices as parameters in the default context.

--- yamoda/importer/mira_single.py
+++ yamoda/importer/mira_single.py
@@ -7,6 +7,7 @@ Importer for MIRA single counter data.
 """
 
 import time
+import textwrap
 from os import path
 from datetime import datetime
 
@@ -14,7 +15,7 @@ import numpy
 
 from yamoda.importer.base import ImporterBase, ParsingError, ImportEntry
 from yamoda.server.database import Context, Parameter
-    
+
 
 class Importer(ImporterBase):
     def __init__(self, target):
@@ -75,7 +76,57 @@ class Importer(ImporterBase):
     @classmethod
     def default_context(cls):
         """Creates a `Mira singlecounter` context object."""
-        # TODO Implement complete context initialisation.
-        mira_brief = 'Mira singlecounter.'
-        mira_single = Context(name='Mira singlecounter', brief=mira_brief)
+        mira_brief = 'Data from MIRA single-counter data files.'
+        mira_desc = textwrap.dedent('''\
+        The MIRA all-purpose instrument at the FRM II.
+
+        This context is for data measured using a single counter, as opposed to
+        a PSD detector.
+        ''')
+        mira_single = Context(name='Mira singlecounter', brief=mira_brief,
+                              description=mira_desc)
+        for parname, (parbrief, parunit) in MIRA_PARAMS.iteritems():
+            par_value = Parameter(name=parname, brief=parbrief, unit=parunit)
+            par_status = Parameter(name=parname + '_status',
+                                   brief='Status of %s' % parname)
+            par_offset = Parameter(name=parname + '_offset',
+                                   brief='Offset of %s' % parname, unit=parunit)
+            par_precision = Parameter(name=parname + '_precision',
+                                      brief='Precision of %s' % parname,
+                                      unit=parunit)
+            mira_single.parameters.extend([par_value, par_status,
+                                           par_offset, par_precision])
         return mira_single
+
+
+MIRA_PARAMS = {
+    'mth':  ('MIRA1 Monochromator theta angle'       , 'deg'),
+    'mtt':  ('MIRA1 Monochromator two-theta angle'   , 'deg'),
+    'mgx':  ('MIRA1 Monochromator tilt'              , 'deg'),
+    'mtx':  ('MIRA1 Monochromator translation X'     , 'mm'),
+    'mty':  ('MIRA1 Monochromator translation Y'     , 'mm'),
+    'm2th': ('MIRA2 Monochromator theta angle'       , 'deg'),
+    'm2tt': ('MIRA2 Monochromator two-theta angle'   , 'deg'),
+    'm2gx': ('MIRA2 Monochromator tilt'              , 'deg'),
+    'm2tx': ('MIRA2 Monochromator translation X'     , 'mm'),
+    'm2ty': ('MIRA2 Monochromator translation Y'     , 'mm'),
+    'mono': ('Monochromator (ki)'                    , 'A-1'),
+    'om':   ('Sample theta angle'                    , 'deg'),
+    'phi':  ('Sample two-theta angle'                , 'deg'),
+    'sgx':  ('Sample goniometer around X axis'       , 'deg'),
+    'sgy':  ('Sample goniometer around Y axis'       , 'deg'),
+    'stx':  ('Sample translation X (along the beam)' , 'mm'),
+    'sty':  ('Sample translation Y (horizontal)'     , 'mm'),
+    'stz':  ('Sample translation Z (vertical)'       , 'mm'),
+    'srot': ('Sample rotation inside cryostat'       , 'deg'),
+    'ath':  ('Analyser theta angle'                  , 'deg'),
+    'att':  ('Analyser two-theta angle'              , 'deg'),
+    'ana':  ('Analyser (kf)'                         , 'A-1'),
+    'mira': ('MIRA triple-axis device'               , 'rlu/meV'),
+    'ms2':  ('Slit after the monochromator'          , 'mm'),
+    'ss1':  ('Sample slit 1'                         , 'mm'),
+    'ss2':  ('Sample slit 2'                         , 'mm'),
+    'T':    ('Sample temperature'                    , 'K'),
+    'I':    ('Current in electromagnet'              , 'A'),
+    'B':    ('Magnetic field'                        , 'T'),
+}
