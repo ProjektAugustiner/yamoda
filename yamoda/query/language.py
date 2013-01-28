c709f3827a9ec2110aa61576705732399825aa53
tobixx0 <Tobi.Stenzel@gmx.de>
2013-01-28 04:56:28 +0100

    Added description to parcon regexes, fixed SQLAlchemy version

--- yamoda/query/language.py
+++ yamoda/query/language.py
@@ -35,18 +35,18 @@ def _concat_list(args):
 
 ##### parsers (rules)
 
-### names
+# ## names
 context_name = Word(string.letters + string.digits + "_")
 data_name = context_name
 param_name = context_name(desc="param_name")
 user_name = context_name(description="user name")
 
-### number literals
+# ## number literals
 
 #  a floating point number like in Python
-float_lit = Regex(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?")[float](name="float lit regex")
+float_lit = Expected(Regex(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?")[float](name="float lit regex"), "'floating point number'")
 
-### misc
+# ## misc
 
 int_lit = Word(string.digits)[int]
 
@@ -64,7 +64,7 @@ param_cond = interval | comparison
 
 filter_expr = (param_cond + ZeroOrMore(L("or") + param_cond))[_concat_list]
 
-### clauses
+# ## clauses
 
 sort_spec = L("sort") + ":" + OneOrMore(sort_param)
 
@@ -81,7 +81,7 @@ user_spec = (L("user") + ":" + user_name)[UserRepr]
 
 # Date range like: '12 August 2012 to 12 December 2012'
 # XXX: we could allow more date range formats because daterangeparser understands many other formats
-date_interval = Regex(r"\d{1,2} \w+ \d{2,4} to \d\d \w+ \d{2,4}")
+date_interval = Expected(Regex(r"\d{1,2} \w+ \d{2,4} to \d\d \w+ \d{2,4}"), "'date string (like >11 August 2011 to 12 September 2012<)'")
 
 creation_time_spec = (L("created") + ":" + date_interval)[lambda t: TimeInterval(*daterangeparser.parse(t))]
 
